7be04630d0d2bdaf3508658a9a32bb51f292ffd4
Alexander Koryttsev <akoryttsev@gmail.com>
2021-09-10 17:01:12 +0300

    Improve and fix SQL

--- sources/services/localDB/sqlite.ts
+++ sources/services/localDB/sqlite.ts
@@ -3,19 +3,26 @@ import { Profile } from 'shared/types'
 
 const db = openDatabase('db')
 
+type Notted<T extends string> = T | `NOT ${T}`
+
 type BasicOperator = '=' | '>' | '<' | '>=' | '<=' | '<>'
-type MultiOperator = 'BETWEEN' | 'NOT BETWEEN' | 'IN' | 'NOT IN'
-type LikeOperator = 'LIKE' | 'NOT LIKE'
-type Operator = BasicOperator | MultiOperator | LikeOperator
+type BetweenOperator = Notted<'BETWEEN'>
+type InOperator = Notted<'IN'>
+type LikeOperator = Notted<'LIKE'>
+type IsOperator = 'IS'
+type IsValue = Notted<'NULL'>
+type Operator = BasicOperator | InOperator | LikeOperator | BetweenOperator | IsOperator
+type ColumnTypes = number | string | boolean
+
+type OnlyAllowedTypes<T> = { [P in keyof T as T[P] extends ColumnTypes ? P : never]: T[P] }
 
 type WhereItem<T> = {
 	key: keyof T
 	operator: Operator
 	value: any
-	not?: true
 }
 
-class Query<T> {
+class Query<A, T = OnlyAllowedTypes<A>> {
 	props: (keyof T)[] = []
 	private whereItems: WhereItem<T>[] = []
 	readonly table: string
@@ -32,8 +39,9 @@ class Query<T> {
 				  this.whereItems
 						.map((i) => {
 							const string = i.key + ' ' + i.operator + ' '
-							if (i.operator === 'BETWEEN') return string + i.value[0] + ' AND ' + i.value[1]
-							else if (i.operator === 'IN') return string + '(' + i.value.join(',') + ')'
+							if (i.operator.includes('BETWEEN')) return string + i.value[0] + ' AND ' + i.value[1]
+							else if (i.operator.includes('IN')) return string + "('" + i.value.join("','") + "')"
+							else if (i.operator.includes('LIKE')) return string + "'" + i.value + "'"
 							else return string + i.value
 						})
 						.join(' AND ')
@@ -41,10 +49,18 @@ class Query<T> {
 		)
 	}
 
-	where = <K extends keyof T, O extends Operator, V = T[K]>(
-		key: K,
+	where = <P extends keyof T, O extends Operator, V extends T[P]>(
+		key: P,
 		operator: O,
-		value: O extends MultiOperator ? V[] : O extends LikeOperator ? string : V,
+		value: O extends InOperator
+			? V[]
+			: O extends BetweenOperator
+			? [V, V]
+			: O extends LikeOperator
+			? string
+			: O extends IsOperator
+			? IsValue
+			: V,
 	) => {
 		this.whereItems.push({ key, operator, value })
 		return { and: this.where }
@@ -53,4 +69,10 @@ class Query<T> {
 
 const q = new Query<Profile>('some_table')
 q.props = ['firstName', 'id']
-q.where('firstName', 'NOT LIKE', 's%').and('age', '>=', 22).and('age', 'NOT BETWEEN', [1, 2])
+q.where('firstName', 'NOT LIKE', 's%')
+	.and('age', '>=', 22)
+	.and('age', 'NOT BETWEEN', [1, 2])
+	.and('lastName', 'IN', ['Alex', 'Julia'])
+	.and('male', '=', true)
+	.and('id', '=', 'ffew')
+	.and('lastName', 'IS', 'NULL')
